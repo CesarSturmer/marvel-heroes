749bd8661947da8d9c2e1fe9f5d0fc77038a1d26
Cesar Sturmer <cesar_dudutm@hotmail.com>
2022-06-29 20:04:03 -0300

    configuração do styled componentes, rotas e estrutura base do projeto

---
+++ src/styles/theme.ts
@@ -0,0 +1,19 @@
+export default {
+  breakpoints: {
+    mobile: "max-width: 480px",
+    tablet: "max-width: 820px",
+    desktop: "max-width: 1200px",
+  },
+
+  colors: {},
+  comom: {
+    black: "#000",
+    white: "#FFFF",
+  },
+};
+
+// até 480 mobile pequeno
+// até 820 mobile grande
+// ate 1200 tablet
+// maior que 1200 desktop
+// máximo para tablet é de 820 => 1200 ( @media)
